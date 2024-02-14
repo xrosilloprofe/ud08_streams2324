264aa310e77c2f2649229a5b8b37882db153010c
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-02-14 17:21:54 +0100

    20240214

--- src/main/java/es/ieslavereda/CopyBytes.java
+++ src/main/java/es/ieslavereda/CopyBytes.java
@@ -1,7 +1,31 @@
 package es.ieslavereda;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class CopyBytes {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try{
+            fis = new FileInputStream("Jack_Russell_Terrier_1.jpg");
+            fos = new FileOutputStream("copia.jpg");
+
+            int copia;
+
+            while((copia=fis.read())!=-1)
+                fos.write(copia);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if(fis!=null)
+                fis.close();
+            if(fos!=null)
+                fos.close();
+        }
 
     }
 }
